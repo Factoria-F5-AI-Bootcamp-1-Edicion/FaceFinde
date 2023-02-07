fb86fa8130d64f7ae38c8aa93ae81b564a5b6708
Victor Arbiol <aratancoders.blockchain@skiff.com>
2023-02-07 09:15:59 +0100

    Add files via upload

---
+++ FaceVerify/README.md
@@ -0,0 +1,41 @@
+# Face Detection Application using Python and Artificial Vision
+
+![Application Screenshot](https://github.com/aratan/idfaceml/blob/main/Captura%20de%20pantalla_2023-01-21_12-28-50.png?raw=true)
+
+## Introduction
+
+This application uses Python and Artificial Vision to detect faces in video. It utilizes the OpenCV library for image processing and the Haar Cascade classifier for detection.
+
+## Usage
+
+1. Run the script
+2. Select an image file
+3. The application will display the image with bounding boxes around any detected faces
+
+## Requirements
+
+- Python 3
+- OpenCV
+- Face_recognition
+- Numpy
+- OS
+
+## Installation
+0. conda env create -n FaceFind --file environment.yml
+1. Clone or download the repository
+2. Install the required libraries by running `pip install -r requirements.txt`
+3. Run the script with `python main.py`
+
+## Conclusion
+
+This application demonstrates the capabilities of Python and Artificial Vision in detecting faces in images. It can be used as a starting point for more advanced projects in computer vision and image processing.
+
+
+# The Team: AratanCoders
+Víctor Arbiol
+
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+
+[![Imports: isort](https://img.shields.io/badge/%20imports-isort-%231674b1?style=flat&labelColor=ef8336)](https://pycqa.github.io/isort/)
+
+[![RESULT: PASSED (minimum: 80.0%, actual: 100.0%)](https://img.shields.io/badge/interrogate-80%25%20--%20100%25%20PASS-green)
---
+++ FaceVerify/main.py
@@ -0,0 +1,86 @@
+# por el terminal externo funciona perfecto
+# import cv2
+# img = cv2.imread("./image.jpg")
+# print(img)
+## pip install dlib-19.22.99-cp39-cp39-win_amd64.whl
+''' https://github.com/tobyyosoba777/Computer-Vision-OpenCv-Python/blob/main/Libraries/dlib-19.22.99-cp39-cp39-win_amd64.whl
+'''
+import os
+import cv2
+import face_recognition
+import numpy as np
+from deepface import DeepFace
+
+# Variables de caras encodadas y nombres de las caras
+encodings_caras = []
+nombres_caras = []
+
+# Directorio actual
+directorio = os.getcwd()
+path_caras = os.path.join(directorio, "caras/")
+
+# Obtiene lista de fotos de caras en el directorio
+fotos_caras = [f for f in os.listdir(path_caras) if f.endswith(".jpg")]
+
+# Entrena caras
+for foto in fotos_caras:
+    # Carga imagen de cara y obtiene encoding
+    imagen = face_recognition.load_image_file(os.path.join(path_caras, foto))
+    encoding = face_recognition.face_encodings(imagen)[0]
+    encodings_caras.append(encoding)
+    nombres_caras.append(foto)
+
+# Inicia captura de video
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Obtiene frame de video
+    ret, frame = video_capture.read()
+    # Redimensiona el frame
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    # Convierte a RGB
+    rgb_small_frame = small_frame[:, :, ::-1]
+
+    # Encuentra locaciones de caras y encodings en el frame
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+    nombres_detectados = []
+
+    # Compara encodings de caras detectadas con encodings entrenadas
+    for encoding in face_encodings:
+        matches = face_recognition.compare_faces(encodings_caras, encoding)
+        nombre = "Desconocido"
+        distancias_cara = face_recognition.face_distance(encodings_caras, encoding)
+        mejor_coincidencia = np.argmin(distancias_cara)
+
+        if matches[mejor_coincidencia]:
+            nombre = nombres_caras[mejor_coincidencia]
+        nombres_detectados.append(nombre)
+        ## aqui la emocion y for para != lineas
+
+    # Dibuja rectángulos y nombres en el frame
+    for (top, derecha, abajo, izquierda), nombre in zip(
+        face_locations, nombres_detectados
+    ):
+        top *= 4
+        derecha *= 4
+        abajo *= 4
+        izquierda *= 4
+
+        cv2.rectangle(frame, (izquierda, top), (derecha, abajo), (0, 0, 255), 2)
+
+        cv2.rectangle(
+            frame, (izquierda, abajo - 35), (derecha, abajo), (0, 0, 255), cv2.FILLED
+        )
+
+    fuente = cv2.FONT_HERSHEY_DUPLEX
+
+    cv2.putText(
+        frame, nombre, (izquierda + 6, abajo - 6), fuente, 1.0, (255, 255, 255), 1
+    )
+
+    # Muestra frame con caras reconocidas
+    cv2.imshow("Video", frame)
+    # Si se presiona 'q', sale del ciclo
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
